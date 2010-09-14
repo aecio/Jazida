a8ea67efef736d338e21b1b1e94ebd256d44cac7
aecio <aecio@73128902-11a4-f64b-800a-f0fca4e90f86>
2010-09-14 12:54:30 +0000

    Adição de método utilitário para converter listas de Text para listas de String
    git-svn-id: svn://lapesi.cefetpi.br/jazida@33 73128902-11a4-f64b-800a-f0fca4e90f86

--- src/br/edu/ifpi/jazida/util/WritableUtils.java
+++ src/br/edu/ifpi/jazida/util/WritableUtils.java
@@ -1,11 +1,14 @@
 package br.edu.ifpi.jazida.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
 import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 
 public class WritableUtils {
@@ -23,5 +26,20 @@ public class WritableUtils {
 			novoMapa.put(each.getKey().toString(), each.getValue().toString());
 		}
 		return novoMapa;
+	}
+
+	
+	/**
+	 * Converte uma Lista de {@link Text} para uma lista de {@link String}.
+	 * 
+	 * @param lista Um lista do tipo {@link Text}s.
+	 * @return Uma lista de {@link String}s equivalente à recebida.
+	 */
+	public static List<String> convertTextListToStringList(List<Text> lista) {
+		List<String> novaLista = new ArrayList<String>();
+		for (Text text : lista) {
+			novaLista.add(text.toString());
+		}
+		return novaLista;
 	}
 }
--- test/br/edu/ifpi/jazida/util/WritableUtilsTest.java
+++ test/br/edu/ifpi/jazida/util/WritableUtilsTest.java
@@ -1,13 +1,17 @@
 package br.edu.ifpi.jazida.util;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
 import org.junit.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 
 public class WritableUtilsTest {
 
@@ -17,10 +21,26 @@ public class WritableUtilsTest {
 		MapWritable mapWritable = new MapWritable();
 		mapWritable.put(new Text("chave"), new Text("valor"));
 		// Quando
-		Map<String, String> map = WritableUtils.convertMapWritableToMap(mapWritable);
+		Map<String, String> map = WritableUtils
+				.convertMapWritableToMap(mapWritable);
 		// Então
 		final String valor = (String) map.get("chave");
 		assertThat(valor, is(equalTo("valor")));
+	}
+
+	@Test
+	public void deveriaConverterUmaListaDeTextParaListaDeStrings() {
+		// dado
+		List<Text> lista = new ArrayList<Text>();
+		lista.add(new Text("texto1"));
+		lista.add(new Text("texto2"));
+		lista.add(new Text("texto3"));
+		
+		// quando
+		List<String> novaLista = WritableUtils.convertTextListToStringList(lista);
+		
+		// então
+		assertThat(novaLista, contains("texto1","texto2","texto3"));
 	}
 
 }
